e7997456cd0f708a96dd1a989691c6adda868fb2
marvinbra <marvinbopp37@gmail.com>
2023-12-05 22:05:49 +0100

    data
    data of vacations in js

---
+++ data.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: "1",
+        title: "Amsterdam",
+        location: "Netherlands",
+        googleMapsUrl: "https://maps.app.goo.gl/j3Wdo9d3aBt7VV6g9",
+        startDate: "11 April, 2023",
+        endDate: "15 April, 2023",
+        description: "Amsterdam is globally renowned for its liberal approach to cannabis, providing a unique atmosphere where the legal consumption of marijuana in designated coffee shops contributes to the city's distinct cultural landscape.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id: "2",
+        title: "São Paulo",
+        location: "Brasil",
+        googleMapsUrl: "https://maps.app.goo.gl/ABqp6h9KKyTukKpC7",
+        startDate: "8 Aug, 2022",
+        endDate: "18 Dec, 2022",
+        description: "São Paulo, a vibrant metropolis at the heart of Brazil, pulsates with the exhilarating rhythm of its diverse and beautiful culture, embodying the dynamic energy and colorful craziness that make it a captivating urban experience..",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id: "3",
+        title: "Jerusalem",
+        location: "Israel",
+        googleMapsUrl: "https://maps.app.goo.gl/9vfFvwisMeYCahGR7",
+        startDate: "14 Jun, 2021",
+        endDate: "21 Jun, 2022",
+        description: "Jerusalem today resonates with ancient vibes, where historic landmarks, religious significance, and a rich cultural tapestry converge to create an atmosphere steeped in antiquity.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id: "4",
+        title: "Rom",
+        location: "Italy",
+        googleMapsUrl: "https://maps.app.goo.gl/j3Wdo9d3aBt7VV6g9",
+        startDate: "20 Aug, 2021",
+        endDate: "4 Sep, 2021",
+        description: "Rome, with its vibrant palette of colors bathed in delightful weather, is a city where the ancient Colosseum stands as a majestic centerpiece, embodying the timeless allure of history and architectural splendor.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id: "5",
+        title: "Barcelona",
+        location: "Spain",
+        googleMapsUrl: "https://maps.app.goo.gl/wPexX7kRVNt123CQ9",
+        startDate: "12 Aug, 2023",
+        endDate: "25 Aug, 2021",
+        description: "Barcelona, a city pulsating with wild energy, seamlessly blends urban vibrancy with the laid-back allure of its beaches, creating a dynamic atmosphere where the lively streets meet the calming waves.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    }
+]
